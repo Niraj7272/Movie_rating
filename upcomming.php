74a7024e2cea64d0a2a4dc0c0bdd0fc2657d74bf
Niraj Chaudhary <84075393+Niraj7272@users.noreply.github.com>
2024-09-19 13:33:42 +0545

    flex change  to grid

--- upcomming.php
+++ upcomming.php
@@ -7,47 +7,121 @@
     <link rel="stylesheet" href="index1.css">
 </head>
 <body>
-    <!-- hello bca -->
     <div class="container">
         <div class="card">
             <img class="background" src="otherpic/poster1.jpeg" alt="Game Poster">
             <div class="card-content">
-                <h3 class="title">God of War</h3>
+                <h3 class="title">6.2/10</h3><br>
+                <h3 class="title">God of War</h3><br>
+                <a href="details.php"><button name="details_btn" class="button-title"><b>Details</b></button></a>
             </div>
             <div class="backdrop"></div>
         </div>
         <div class="card">
             <img class="background" src="otherpic/poster2.jpeg" alt="Game Poster">
             <div class="card-content">
-                <h3 class="title">Marvel's Spider-Man</h3>
+                <h3 class="title">6.2/10</h3><br>
+                <h3 class="title">Marvel's Spider-Man</h3><br>
+                <a href="details.php"><button name="details_btn" class="button-title"><b>Details</b></button></a>
             </div>
             <div class="backdrop"></div>
         </div>
-        <!-- <div class="card">
+        <div class="card">
             <img class="background" src="otherpic/poster3.jpeg" alt="Game Poster">
             <div class="card-content">
-                <h3 class="title">The Last of Us</h3>
+                <h3 class="title">6.2/10</h3><br>
+                <h3 class="title">The Last of Us</h3><br>
+                <a href="details.php"><button name="details_btn" class="button-title"><b>Details</b></button></a>
             </div>
             <div class="backdrop"></div>
-        </div> -->
+        </div>
         <div class="card">
             <img class="background" src="otherpic/poster4.jpeg" alt="Game Poster">
             <div class="card-content">
-                <h3 class="title">Uncharted</h3>
+                <h3 class="title">6.2/10</h3><br>
+                <h3 class="title">Uncharted</h3><br>
+                <a href="details.php"><button name="details_btn" class="button-title"><b>Details</b></button></a>
             </div>
             <div class="backdrop"></div>
         </div>
-        <!-- <div class="card">
+        <div class="card">
             <img class="background" src="otherpic/poster5.jpeg" alt="Game Poster">
             <div class="card-content">
-                <h3 class="title">Uncharted</h3>
+                <h3 class="title">6.2/10</h3><br>
+                <h3 class="title">Uncharted</h3><br>
+                <a href="details.php"><button name="details_btn" class="button-title"><b>Details</b></button></a>
             </div>
             <div class="backdrop"></div>
-        </div> -->
+        </div>
         <div class="card">
             <img class="background" src="otherpic/poster6.jpeg" alt="Game Poster">
             <div class="card-content">
-                <h3 class="title">Out Sider</h3>
+                <h3 class="title">6.2/10</h3><br>
+                <h3 class="title">Out Sider</h3><br>
+                <a href="details.php"><button name="details_btn" class="button-title"><b>Details</b></button></a>
+            </div>
+            <div class="backdrop"></div>
+        </div>
+        <div class="card">
+            <img class="background" src="otherpic/poster5.jpeg" alt="Game Poster">
+            <div class="card-content">
+                <h3 class="title">6.2/10</h3><br>
+                <h3 class="title">Uncharted</h3><br>
+                <a href="details.php"><button name="details_btn" class="button-title"><b>Details</b></button></a>
+            </div>
+            <div class="backdrop"></div>
+        </div>
+        <div class="card">
+            <img class="background" src="otherpic/poster5.jpeg" alt="Game Poster">
+            <div class="card-content">
+                <h3 class="title">6.2/10</h3><br>
+                <h3 class="title">Uncharted</h3><br>
+                <a href="details.php"><button name="details_btn" class="button-title"><b>Details</b></button></a>
+            </div>
+            <div class="backdrop"></div>
+        </div>
+        <div class="card">
+            <img class="background" src="otherpic/poster5.jpeg" alt="Game Poster">
+            <div class="card-content">
+                <h3 class="title">6.2/10</h3><br>
+                <h3 class="title">Uncharted</h3><br>
+                <a href="details.php"><button name="details_btn" class="button-title"><b>Details</b></button></a>
+            </div>
+            <div class="backdrop"></div>
+        </div>
+        <div class="card">
+            <img class="background" src="otherpic/poster5.jpeg" alt="Game Poster">
+            <div class="card-content">
+                <h3 class="title">6.2/10</h3><br>
+                <h3 class="title">Uncharted</h3><br>
+                <a href="details.php"><button name="details_btn" class="button-title"><b>Details</b></button></a>
+            </div>
+            <div class="backdrop"></div>
+        </div>
+        <div class="card">
+            <img class="background" src="otherpic/poster5.jpeg" alt="Game Poster">
+            <div class="card-content">
+                <h3 class="title">6.2/10</h3><br>
+                <h3 class="title">Uncharted</h3><br>
+                <a href="details.php"><button name="details_btn" class="button-title"><b>Details</b></button></a>
+            </div>
+            <div class="backdrop"></div>
+        </div>
+        <div class="card">
+            <img class="background" src="otherpic/poster5.jpeg" alt="Game Poster">
+            <div class="card-content">
+                <h3 class="title">6.2/10</h3><br>
+                <h3 class="title">Uncharted</h3><br>
+                <a href="details.php"><button name="details_btn" class="button-title"><b>Details</b></button></a>
+            </div>
+            <div class="backdrop"></div>
+        </div>
+        <div class="card">
+            <img class="background" src="otherpic/poster5.jpeg" alt="Game Poster">
+            <div class="card-content">
+                <h3 class="title">6.2/10</h3><br>
+                <h3 class="title">Uncharted</h3><br>
+                <a href="details.php"><button name="details_btn" class="button-title"><b>Details</b></button></a>
             </div>
             <div class="backdrop"></div>
         </div>
